d8b10ff834d2d561645655affd82d9dd15d07e5d
SanAndreasP <1294@live.de>
2015-05-18 21:02:46 +0200

    updated version

--- java/de/sanandrew/mods/varietychests/util/VarietyChests.java
+++ java/de/sanandrew/mods/varietychests/util/VarietyChests.java
@@ -43,7 +43,7 @@ public final class VarietyChests
 {
     public static final String MOD_ID = "varietychests";
     public static final String MOD_DEPS = "required-after:sapmanpack@[2.4.0,);after:ExtrabiomesXL;after:NotEnoughItems";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
     public static final String PROXY_CLIENT = "de.sanandrew.mods.varietychests.client.util.ClientProxy";
     public static final String PROXY_SERVER = "de.sanandrew.mods.varietychests.util.CommonProxy";
 
